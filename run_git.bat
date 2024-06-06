dd6f1c48e085f23fc92e855fa4c6a6a65ae55e6e
XeBoLaX <xebolax@gmail.com>
2024-06-06 21:43:45 +0200

    Versi├│n 2024.06.06-Inicial-1

--- run_git.bat
+++ run_git.bat
@@ -4,7 +4,7 @@ scons --clean
 scons pot
 git init
 git add --all
-git commit -m "Versión 2024.06.06-Inicial"
+git commit -m "Versión 2024.06.06-Inicial-1"
 git push -u origin master
 git tag 2024.06.06
 git push --tags
