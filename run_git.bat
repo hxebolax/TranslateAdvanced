ef9ecffa11f380015c41bec36018776e99c3cffe
XeBoLaX <xebolax@gmail.com>
2024-06-23 13:59:33 +0200

    Versi├│n 2024.06.23.release1

--- run_git.bat
+++ run_git.bat
@@ -3,7 +3,7 @@
 scons --clean
 git init
 git add --all
-git commit -m "Versión 2024.06.23.release"
+git commit -m "Versión 2024.06.23.release1"
 git push -u origin master
 git tag 2024.06.23
 git push --tags
